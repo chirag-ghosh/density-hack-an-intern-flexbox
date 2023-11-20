73007a1dfd6e40b37c1a620bbe94c42bedf5b0fd
Chirag Ghosh <cghosh828049@gmail.com>
2023-11-21 03:00:20 +0530

    hosting

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
 // const auth = require('./routes/routes')
 // Routes
-const userRoute = require('./routes/userRoutes')
-const orderBookRoute = require('./routes/orderBookRoutes')
-const transactionRoute = require('./routes/transactionRoutes')
+const userRoute = require("./routes/userRoutes");
+const orderBookRoute = require("./routes/orderBookRoutes");
+const transactionRoute = require("./routes/transactionRoutes");
 
+app.use(express.json());
 
-app.use(express.json())
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use('/users', userRoute);
+app.use("/users", userRoute);
 
-app.use('/book', orderBookRoute);
+app.use("/book", orderBookRoute);
 
-app.use('/transaction', transactionRoute);
+app.use("/transaction", transactionRoute);
 
 app.get(`/health`, (req, res) => {
-    res.send(`User App Running at port : ${port}`);
-    console.log(`User App Running at port : ${port}`);
+  res.send(`User App Running at port : ${port}`);
+  console.log(`User App Running at port : ${port}`);
 });
-
-
 
 const port = 5001;
 
 app.listen(port, () => {
-    console.log("Trader Abhinav's ship has set sail from port: 5001")
+  console.log("Trader Abhinav's ship has set sail from port: 5001");
 });
-
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 import Portfolio from "./components/PortfolioPage/Portfolio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import OrderPage from './components/OrderPage/OrderPage';
-import UserState from './Context/UserState';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import OrderPage from "./components/OrderPage/OrderPage";
+import UserState from "./Context/UserState";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://ec2-13-232-154-171.ap-south-1.compute.amazonaws.com/";
 
 function App() {
-
   return (
     <div className="App">
       <UserState>
         <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Portfolio />} />
-          <Route path='/order' element={<OrderPage />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Portfolio />} />
+            <Route path="/order" element={<OrderPage />} />
+          </Routes>
         </BrowserRouter>
-        <ToastContainer 
+        <ToastContainer
           position="top-right"
           autoClose={5000}
           hideProgressBar={false}
